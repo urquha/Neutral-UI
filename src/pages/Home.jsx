28f16ca05da278ee6047537b5370533ef6586e65
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2020-02-21 14:56:44 +0000

    Home page and basic page functionality added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Home() {
+    return (
+        <div>
+            <h1>Yo yo yo</h1>
+            <button>Dont Click Me Boyo</button>
+        </div>
+    )
+}
+
+export default Home
